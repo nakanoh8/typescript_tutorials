af199d2646b10cf31654bc575eec589eaf7e3766
nakanoh <murry.xxx.7@gmail.com>
2020-08-03 23:42:07 +0900

    first commit

---
+++ five_minutes/greeter.js
@@ -0,0 +1,16 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+var user = new Student("Jane", "M.", "User");
+// let user = [0, 1, 2];
+document.body.textContent = greeter(user);
+// console.log(user);
